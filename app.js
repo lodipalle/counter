ee420a55e0a1c6396efe4ab8c71296abca3f0f56
lodi palle <lodipalle@gmail.com>
2020-10-02 16:38:52 +1000

    added colors to counter buttons

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ const value = document.querySelector("#value");
 const btn = document.querySelectorAll(".btn");
 
 btn.forEach(function (btn) {
-  btn.addEventListener('click', function(e){
-    console.log(e.currentTarget.classList);
+  btn.addEventListener("click", function (e) {
+    const trigger = e.currentTarget.classList;
+    if (trigger.contains("decrease")) {
+      count--;
+    }
+    else if (trigger.contains("increase")) {
+      count++;
+    }
+    else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "orange";
+    }
+    if (count === 0) {
+      value.style.color = "black";
+    }
+    value.textContent = count;
   });
 });
